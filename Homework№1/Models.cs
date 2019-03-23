deb1161a1aa9cf8d0d25dbcbf5e0b6be569fd4f5
AleksandrKar <AlreksandrKar7@gmail.com>
2019-03-23 13:58:31 +0200

    Entity Framework
    Задание
    
    Используя VisualStudio, создайте проект по шаблону ConsoleApplication.
    
    Требуется:
    
    Используя подход CodeFirst создайте две сущности с произвольными именами. Свяжите две сущности связью один ко многим. Заполните сущности данными. Используйте FluentAPI для настройки модели. Используйте асинхронные методы, а также используйте инициализацию DropCreateDatabaseAlways. Выведите данные в консоль/asp.net страницу.

---
+++ Homework№1/Models.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    public class Students
+    {
+        [Key]
+        [DisplayName("Индекс")]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("ФИО")]
+        public string FullName { get; set; }
+
+        [Required]
+        [DisplayName("Группа")]
+        virtual public Groups Group { get;set;}
+    }
+
+    public class Groups
+    {
+        [Key]
+        [DisplayName("Индекс")]
+        public int Id { get; set; }
+
+        [DisplayName("Название")]
+        public string Name { get; set; }
+    }
+
+    public class UniversityDbContext : DbContext
+    {
+        public DbSet<Students> Students { get; set; }
+        public DbSet<Groups> Groups { get; set; }
+
+        public UniversityDbContext()
+            : base("UniversityDb")
+        {
+
+        }
+
+        static UniversityDbContext()
+        {
+            Database.SetInitializer(new UniversityContextInitializer());
+        }
+    }
+
+    class UniversityContextInitializer : DropCreateDatabaseAlways<UniversityDbContext>
+    {
+        protected override void Seed(UniversityDbContext db)
+        {
+            Groups group1 = new Groups { Name = "151-18ck-1" };
+            Groups group2 = new Groups { Name = "E-14 1/9" };
+
+            var students = new List<Students>
+            {
+                new Students() { FullName = "Karpov Aleksandr Vladimirovich", Group = group1}
+                , new Students() { FullName = "Petrov Petr Petrovovich", Group = group2}
+                , new Students() { FullName = "Vaskin Vasiliy Vasilievich", Group = group1}
+                , new Students() { FullName = "Vovkow Vowa Vowovich", Group = group2 }
+            };
+
+            db.Groups.AddRange(new List<Groups>(){ group1, group2 });
+            db.Students.AddRange(students);
+
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Homework№1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowAllStudentsAsync();
+            Console.ReadLine();
+        }
+
+        private static async void ShowAllStudentsAsync()
+        {
+            List<StudentInfo> studentList = await Task.Run(() => GetAllStudents());
+
+            foreach (StudentInfo info in studentList)
+            {
+                Console.WriteLine(info.FullName + " " + info.Group);
+            }
+        }
+
+        private static List<StudentInfo> GetAllStudents()
+        {
+        using (UniversityDbContext context = new UniversityDbContext())
+        {
+            List<StudentInfo> studentList = (from student in context.Students
+                                                  join grou in context.Groups
+                                                  on student.Group.Id equals grou.Id
+                                                  select new StudentInfo
+                                                  {
+                                                      FullName = student.FullName
+                                                      ,
+                                                      Group = grou.Name
+                                                  }).ToList();
+
+            return studentList;
+            }
+        }
+    }
+
+    class StudentInfo
+    {
+        public string FullName { get; set; }
+
+        public string Group { get; set; }
+    }
+}
